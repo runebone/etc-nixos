dfad84a084c1a2ef7e87dfc0fda68e41bfce445a
Konstantin Runov <runebone1@gmail.com>
2023-12-09 20:41:23 +0300

    make stuff up to date

--- configuration.nix
+++ configuration.nix
@@ -4,20 +4,35 @@
 
 { config, pkgs, ... }:
 
+let
+  unstableTarball =
+    fetchTarball
+    https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
+in
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
+  nixpkgs.config = {
+    packageOverrides = pkgs: with pkgs; {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
+  # networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.enableIPv6 = false;
 
   # Modified by me
   virtualisation.virtualbox.host.enable = true;
@@ -66,6 +81,8 @@
         set cuc
         set colorcolumn=80
 
+        set nohlsearch
+
         inoremap {<CR> {<CR>}<C-o>O
         inoremap [<CR> [<CR>]<C-o>O
         inoremap (<CR> (<CR>)<C-o>O
@@ -76,11 +93,11 @@
   programs.zsh = {
     enable = true;
 
-    ohMyZsh = {
-      enable = true;
+    # ohMyZsh = {
+      # enable = true;
       # theme = "darkblood";
-      theme = "half-life";
-    };
+      # theme = "half-life";
+    # };
   };
 
   environment.variables = {
@@ -129,6 +146,7 @@
     libinput.enable = true;
 
     layout = "us,ru";
+    xkbVariant = ",";
     xkbOptions = "grp:caps_toggle";
 
     displayManager.defaultSession = "none+bspwm";
@@ -152,6 +170,7 @@
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
+  hardware.bluetooth.enable = false;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.human = {
@@ -174,6 +193,13 @@
     neovim
   ];
 
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -185,7 +211,13 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    extraConfig = ''
+      X11Forwarding yes
+    '';
+  };
+  services.blueman.enable = false;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
